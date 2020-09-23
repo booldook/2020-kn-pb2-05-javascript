2d389eed798cdc9067688e8bcc13882a03a5e381
booldook <booldook@gmail.com>
2020-09-23 12:29:48 +0900

    18.ajax

--- js/18.ajax.js
+++ js/18.ajax.js
@@ -1,28 +1,34 @@
-var scores = {
-	students: [
-		{
-			name: "홍 길동",
-			kor: 95,
-			eng: 80,
-			math: 92
-		},
-		{
-			name: "홍 길순",
-			kor: 90,
-			eng: 75,
-			math: 90
-		},
-		{
-			name: "홍 길만",
-			kor: 65,
-			eng: 85,
-			math: 88
-		},
-	],
-	total: 3
+function onAjax() {
+	$.ajax({
+		type: 'get',
+		dataType: 'json',
+		url: '../json/score.json',
+		success: function(r){
+			console.log(r);
+		}
+	});
 }
 
-var scoresJSON = JSON.stringify(scores);
+function onAjaxTeacher() {
+	// $.get(url, sendData, callBack);
+	$.get('http://192.168.0.14:5500/json/score.json', function(r){
+		console.log(r);
+	});
+}
 
-console.log(scores);
-console.log(scoresJSON);
+function onWeather() {
+	var url = 'https://api.openweathermap.org/data/2.5/weather';
+	var data = {
+		appid: '02efdd64bdc14b279bc91d9247db4722',
+		id: '1835848',
+		units: 'metric'
+	};
+	var success = function(r) {
+		console.log(r);
+	}
+	$.get(url, data, success);
+}
+
+$("#btnMy").click(onAjax);
+$("#btnTeacher").click(onAjaxTeacher);
+$("#btnWeather").click(onWeather);
