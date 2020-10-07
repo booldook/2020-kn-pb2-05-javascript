752f6a7557efe28f19d974c3797e803e0c005b29
booldook <booldook@gmail.com>
2020-10-07 13:22:40 +0900

    밥먹고 합시다

--- js/booldook-slide.js
+++ js/booldook-slide.js
@@ -1,25 +1,42 @@
 var Slide = function(arg) {
-	this.slides = arg.slide;
+	this.slide = arg.slide;
+	this.$slides = [];
 	this.container = arg.container;
+	this.$container = $(this.container);
 	this.direction = arg.direction || 'hori';
 	this.autoStart = arg.autoStart || false;
 	this.gapSpeed = arg.gapSpeed || 3000;
 	this.aniSpeed = arg.aniSpeed || 500;
-	$(this.container).css({
+	this.$container.css({
+		"border": "1px solid red",
 		"position": "relative",
 		// "overflow": "hidden"
 	});
-	this.wrapper = $('<div class="booldook-wrapper booldook-'+this.direction+'"></div>').appendTo($(this.container));
+	this.$wrapper = $('<div class="booldook-wrapper booldook-'+this.direction+'"></div>').appendTo(this.$container);
+	this.now = 0;
+	this.last = this.slide.length - 1;
+
 	this.init();
 	return this;
 }
 
 Slide.prototype.init = function(){
 	var html = '';
-	for(var i=0; i<this.slides.length; i++) {
+	for(var i=0; i<this.slide.length; i++) {
 		html  = '<div class="booldook-slide">';
-		html += '<img src="'+this.slides[i]+'" class="img">';
+		html += '<img src="'+this.slide[i]+'" class="img">';
 		html += '</div>';
-		$(this.wrapper).append(html);
+		this.$slides.push($(html).appendTo(this.$wrapper));
 	}
+	if(this.direction === 'hori' || this.direction === 'vert') {
+		this.$slides.push($(this.$slides[0].clone()).appendTo(this.$wrapper));
+		this.last = this.slide.length;
+	}
+	this.$btnPrev = $('<div class="booldook-btn booldook-prev">〈</div>').appendTo(this.$container);
+	this.$btnNext = $('<div class="booldook-btn booldook-next">〉</div>').appendTo(this.$container);
+	this.startInit();
+}
+
+Slide.prototype.startInit = function() {
+
 }
